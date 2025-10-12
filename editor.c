0768a77d688341582bd1fe866495b0ec535ec0d8
ibrkhalid11 <ibrkhalid11@gmail.com>
2025-10-12 17:12:37 -0600

    can read keystrokes from terminal, reads it as its ASCII character and prints that back to the terminal

--- editor.c
+++ editor.c
@@ -1,20 +1,72 @@
-/*for struct termios, tcgetattr, tcsetattr*/
+
+
+/*** includes ***/
+#include <errno.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <termios.h>
-
-
-/*unistd for read function, STDIN_FILENO*/
 #include <unistd.h>
-void enableRawMode(){
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+/*** function definitions ***/
+void enableRawMode(void);
+void disableRawMode(void);
+void die(const char *s);
+
+/*** global variables ***/
+struct termios orig_termios;
+
+/*purpose of die...*/
+void die(const char *s){
+    perror(s);
+    exit(1);
 }
 
+/*purpose of rawmode*/
+void disableRawMode(void){
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+        die("tcsetattr");
+    }
+}
+
+/*purpose of rawmode...*/
+void enableRawMode(void){
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    raw = orig_termios;
+    raw.c_iflag &= (tcflag_t)~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= (tcflag_t)~(OPOST);
+    raw.c_cflag |= (tcflag_t)(CS8);
+    raw.c_lflag &= (tcflag_t)~(ECHO | IEXTEN | ICANON | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){die("tcsetattr");}
+}
+
+
+/*main logic*/
 int main(void){
-    char c;
+
     enableRawMode();
-    while(read(STDIN_FILENO, &c, 1) == 1 && (c != 'q'));
+
+    while(1){
+        char c = '\0';
+        ssize_t nread = read(STDIN_FILENO, &c, 1);
+        if (nread == -1 && errno != EAGAIN) {
+            die("read");
+        } else if (nread == 0) {
+            continue;
+        }
+        if(iscntrl(c)){
+            printf("%d\r\n", c);
+        } else{
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if(c == 'q'){break;}
+    }
     return 0;
 }
 
